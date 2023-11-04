49df63371139b7f9e19172c0b12271c9e7201164
Nilesh <nilesh199721@gmail.com>
2023-11-04 08:36:45 +0530

    updating

--- Script.js
+++ Script.js
@@ -1,9 +1,8 @@
-
 document.getElementById("open-resume-link").addEventListener("click", function (event) {
     event.preventDefault(); // Prevent the link from navigating immediately
-    
-     var confirmation = confirm("Are you sure you want to open the resume?");
-     if (confirmation) {
+
+    var confirmation = confirm("Are you sure you want to open the resume?");
+    if (confirmation) {
         window.location.href = this.getAttribute("href"); // Navigate to the resume link if "Yes" is selected
-     }
-})
+    }
+    });
